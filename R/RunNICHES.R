0bd5ea5992b77dc8c1e50f9ada5aab419eec09de
MSBR <michasam.raredon@yale.edu>
2021-09-22 17:30:29 -0400

    Updates to package name and annotations

--- R/RunNICHES.R
+++ R/RunNICHES.R
@@ -1,31 +1,31 @@
-#' RunSCC
+#' RunNICHES
 #' 
-#' Performs Single-Cell Connectivity (SCC) transformations on a Seurat object. 
+#' Performs user-selected NICHES transformations on a Seurat object. 
 #' By default, references RunCellToCell, RunCellToSystem, and RunSystemToCell functions. 
 #' If positional data is provided, similar analyses can be performed which are limited exclusively to cells that directly neighbor each other.
-#' Output is a set of specialized Seurat objects (SCC objects) containing cell signaling information.
+#' Output is a set of specialized NICHES objects which allow fine analysis of cell-cell interactions.
 #' 
 #' @param object A Seurat 4.0 object. The active identity will be used to define populations for connectomic sampling and crossings.
 #' @param LR.database Accepts either 'fantom5' or a custom data.frame with the first column equal to ligands, second column equal to associated receptors.
 #' @param species The species of the object that is being processed. Only required if LR.database = 'fantom5', and allows 'human','mouse','rat', or 'pig'
-#' @param assay The assay to run the SCC transformation on. Defaults to "RNA."
+#' @param assay The assay to run the NICHES transformation on. Defaults to "RNA."
 #' @param min.cells.per.ident Default 10. A limit on how small (how many cells) a single population can be to participate in connectomic crossings.
-#' @param meta.data.to.map A character vector of metadata names present in the original object which will be carried to the SCC objects
+#' @param meta.data.to.map A character vector of metadata names present in the original object which will be carried to the NICHES objects
 #' @param position.x The name of the meta.data column specifying location on the spatial x-axis. Only relevant for spatial omics data.
 #' @param position.y The name of the meta.data column specifying location on the spatial y-axis. Only relevant for spatial omics data.
 #' @param ... Additional parameters to pass to RunCellToCell, RunSystemToCell, RunCellToSystem, or spatial equivalents
 #' @param rad.set 
-#' @param CellToCell 
-#' @param CellToSystem 
-#' @param SystemToCell 
-#' @param CellToCellSpatial 
-#' @param CellToNeighborhood 
-#' @param NeighborhoodToCell 
+#' @param CellToCell Default TRUE. Whether to analyze cell-cell interactions without considering spatial coordinates.
+#' @param CellToSystem Default FALSE. Whether to analyze summed signaling output to total system coming from each cell. Does not consider Euclidean coordinates.
+#' @param SystemToCell Default FALSE. Whether to analyze summed signaling input from total system landing on each cell (cellular microenvironment/niche). Does not consider Euclidean coordinates. 
+#' @param CellToCellSpatial Default FALSE. Whether to analyze cell-cell interactions between Euclidean neighbors. Only applicable in spatial datasets.
+#' @param CellToNeighborhood Default FALSE. Whether to analyze summed signaling output to Euclidean neighbors. Only applicable in spatial datasets.
+#' @param NeighborhoodToCell Default FALSE. Whether to analyze summed signaling input from Euclidean neighbors (cellular microenvironment/niche). Only applicable in spatial datasets.
 #'
 #' @export
 
 
-RunSCC <- function(object,
+RunNICHES <- function(object,
                         LR.database,
                         species,
                         assay,
@@ -44,13 +44,13 @@ RunSCC <- function(object,
    # Initialize output structure
   output <- list()
   
-  # Calculate SCC organizations without spatial restrictions
+  # Calculate NICHES organizations without spatial restrictions
   
   if (CellToCell == T){output[[length(output)+1]] <- RunCellToCell(object,LR.database,assay = assay,species = species,meta.data.to.map = meta.data.to.map,min.cells.per.ident = min.cells.per.ident,...)}
   if (CellToSystem == T){output[[length(output)+1]] <- RunCellToSystem(object,LR.database,assay = assay,species = species,meta.data.to.map = meta.data.to.map,min.cells.per.ident = min.cells.per.ident,...)}
   if (SystemToCell == T){output[[length(output)+1]] <- RunSystemToCell(object,LR.database,assay = assay,species = species,meta.data.to.map = meta.data.to.map,min.cells.per.ident = min.cells.per.ident,...)}
   
-  # If requested, additionally calculate spatially-limited SCC organizations
+  # If requested, additionally calculate spatially-limited NICHES organizations
   
   if (CellToCellSpatial == T | CellToNeighborhood == T | NeighborhoodToCell == T){
     
