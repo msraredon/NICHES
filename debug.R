ad4ae78c7cea4578c4618e66e318a28ad5895b9f
KevinBastianYang <junchen.k.yang@gmail.com>
2021-10-18 15:15:22 -0400

    delete unused test files

--- debug.R
+++
@@ -1,5 +0,0 @@
-## Debugging the prepSeurat function where nrow(GOI) becomes 0 using the spatial data
-## Finished
-
-##
-
--- debug_remiNders.R
+++
@@ -1,4 +0,0 @@
-# TODO: add the documentations of the parameters of each function
-
-# TODO: allow custom groundtruth and modify the LR.database annotations accordingly
-
