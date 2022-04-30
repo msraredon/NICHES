0178407f5bb5298b534b763bbaed44a1e3c6ae40
KevinBastianYang <junchen.k.yang@gmail.com>
2022-04-29 22:44:51 -0400

    S3 RunNICHES for matrix

--- R/RunNICHES.R
+++ R/RunNICHES.R
@@ -1,3 +1,19 @@
+
+#' RunNICHES 
+#' 
+#' RunNICHES generic function
+#'
+#' @param data normalized data matrix or a seurat object
+#' @param ... 
+#'
+#' @export
+#'
+RunNICHES <- function(data,...){
+  UseMethod("RunNICHES",data)
+}
+
+
+
 #' RunNICHES
 #' 
 #' Performs user-selected NICHES transformations on a Seurat object. 
@@ -28,7 +44,7 @@
 #' @export
 
 
-RunNICHES <- function(object,
+RunNICHES.Seurat <- function(object,
                         assay="RNA",
                         LR.database="fantom5",
                         species,
@@ -122,7 +138,7 @@ RunNICHES <- function(object,
     if(!is.null(rad.set)) rad.set <- as.numeric(rad.set)
   }
   
-  if((!is.null(position.x) | !is.null(position.y)) & org_names_indicator["CellToCellSpatial"] == F & org_names_indicator["CellToNeighborhood"] == F & org_names_indicator["NeighborhoodToCell"] == T)
+  if((!is.null(position.x) | !is.null(position.y)) & org_names_indicator["CellToCellSpatial"] == F & org_names_indicator["CellToNeighborhood"] == F & org_names_indicator["NeighborhoodToCell"] == F)
     warning("Spatial positions are provided but the spatial organization functions: 'CellToCellSpatial','CellToNeighborhood', and 'NeighborhoodToCell' are set to FALSE.")
     
   # TODO: check blend with the system indicators, blend is not in other functions?
@@ -183,6 +199,171 @@ RunNICHES <- function(object,
                                                                                    edgelist = edgelist
                                                                                    )} #Spatially-limited Neighborhood-Cell vectors (niches)
 
+  # jc: Add organization names to the list
+  names(output) <- names(org_names_indicator)[org_names_indicator]
+  
+  # Compile objects for output
+  return(output)
+}
+
+
+# TODO: for now pos.x and pos.y are required to be 2 cols of meta.data.to.map
+#       let's think of better ways 
+RunNICHES.matrix <- function(data_mat,
+                             LR.database="fantom5",
+                             species,
+                             min.cells.per.ident = NULL,
+                             min.cells.per.gene = NULL,
+                             meta.data.to.map = NULL,
+                             position.x = NULL,
+                             position.y = NULL,
+                             custom_LR_database = NULL,
+                             k = 4,
+                             rad.set = NULL,
+                             blend = 'mean',
+                             CellToCell = T,
+                             CellToSystem = F,
+                             SystemToCell = F,
+                             CellToCellSpatial = F,
+                             CellToNeighborhood = F,
+                             NeighborhoodToCell = F
+                            ){
+  # TODO: check the parameter validity here, then register the parameters
+  # 1: check the data format of the required parameters and the optional parameters (if(!is.null())): int, character, etc.
+  # 2. check some of the dependencies of the parameters
+  #   (1) If LR.database is 'custom', then 'custom_LR_database' can't be NULL
+  #   (2) If CellToCellSpatial,CellToNeighborhood, or NeighborhoodToCell is T, then rad.set, 'position.x' and 'position.y' can't be null
+  # 3. check whether other functions have done similar checks
+  # 4. think whether to put stop or warning
+  
+  
+  # check lr database, species, custom_LR_database
+  if(LR.database %in% c("fantom5","omnipath","custom")){
+    if(LR.database == "fantom5"){
+      if(!(species %in% c('human','mouse','rat','pig'))) 
+        stop(paste0("Unsupported species ",species, " for fantom 5 database, only 'human','mouse','rat',or 'pig' supported."))
+    }
+    if(LR.database == "omnipath"){
+      if(!(species %in% c('human','mouse','rat')))
+        stop(paste0("Unsupported species ",species, " for omnipath database, only 'human','mouse',or 'rat' supported."))
+    }
+    if(LR.database == "custom"){
+      # check the format of custom_LR_database
+      if(is.null(custom_LR_database)) stop("custom_LR_database is NULL")
+      # TODO: check gene names
+      message("Custom Ligand Receptor database enabled...")
+      message("Checking the format of the custom database...")
+      if(!is.data.frame(custom_LR_database)){
+        warning("Custom database provided is not in dataframe format.")
+        warning("Converting to dataframe format...")
+        custom_LR_database <- as.data.frame(custom_LR_database)
+      }
+      if(ncol(custom_LR_database) < 2) stop("Custom database provided contains less than 2 columns.")
+    }
+  }else stop('\n LR.receptor argument not recognized. Only accepts "omnipath","fantom5" or "custom".')
+  
+  if(!is.null(custom_LR_database) & LR.database!="custom"){ 
+    warning("custom_LR_database is provided but LR.databse is not specified as 'custom'")
+  }
+  # Convert any non-integer inputs to integers. Still allows NULL as option.
+  if (!is.null(min.cells.per.ident)){
+    min.cells.per.ident <- as.integer(min.cells.per.ident)
+  }
+  if (!is.null(min.cells.per.gene)){
+    min.cells.per.gene <- as.integer(min.cells.per.gene)
+  }
+  
+  # check meta.data.to.map: now it should be a data.frame
+  if(!is.null(meta.data.to.map)){
+    if(!is.data.frame(meta.data.to.map)) meta.data.to.map <- as.data.frame(meta.data.to.map)
+  }
+  
+  # check indicators
+  # jc: Add organization names to the list
+  org_names_indicator <- c(CellToCell,CellToSystem,SystemToCell,CellToCellSpatial,CellToNeighborhood,NeighborhoodToCell)
+  org_names_indicator <- sapply(org_names_indicator,function(org){
+    org_out <- as.logical(org)
+    if(is.na(org_out)) warning("Organization indicator ",org," is not set to TRUE or FALSE.")
+    return(org_out)
+  })
+  names(org_names_indicator) <- c("CellToCell","CellToSystem","SystemToCell","CellToCellSpatial","CellToNeighborhood","NeighborhoodToCell")
+  
+  
+  # If requested, additionally calculate spatially-limited NICHES organizations
+  if (org_names_indicator["CellToCellSpatial"] == T | org_names_indicator["CellToNeighborhood"] == T | org_names_indicator["NeighborhoodToCell"] == T){
+    
+    if (is.null(position.x) | is.null(position.y)){stop("\n Position information not provided. Please specify metadata columns containing x- and y-axis spatial coordinates.")}
+    if(!is.null(k)) k <- as.integer(k)
+    if(!is.null(rad.set)) rad.set <- as.numeric(rad.set)
+  }
+  
+  # TODO: bug fix: change the last logical in the master branch
+  if((!is.null(position.x) | !is.null(position.y)) & org_names_indicator["CellToCellSpatial"] == F & org_names_indicator["CellToNeighborhood"] == F & org_names_indicator["NeighborhoodToCell"] == F)
+    warning("Spatial positions are provided but the spatial organization functions: 'CellToCellSpatial','CellToNeighborhood', and 'NeighborhoodToCell' are set to FALSE.")
+  
+  # TODO: add blend to the spatial case
+  if(org_names_indicator["CellToSystem"] == T | org_names_indicator["SystemToCell"] == T)
+    if(!blend %in% c("sum","mean")) stop("blend paramter is not recognized: need to be 'sum' or 'mean" )
+  
+  # TODO: add a checker to check where position.x and position.y are in the meta data
+  
+  # convert the input data_mat and meta.data.to.map to a Seurat object
+  object <- Seurat::CreateSeuratObject(counts=data_mat,assay="RNA",meta.data=meta.data.to.map, min.cells = 0,min.features = 0)
+  
+  # Initialize output structure
+  output <- list()
+  
+  
+  # jc: move the shared preprocessing steps here to avoid redundancy and reduce the number of parameters to be passed to other functions
+  sys.small <- prepSeurat(object,"RNA",min.cells.per.ident,min.cells.per.gene)
+  ground.truth <- lr_load(LR.database,custom_LR_database,species,rownames(sys.small@assays[[assay]]))
+  if (org_names_indicator["CellToCellSpatial"] == T | org_names_indicator["CellToNeighborhood"] == T | org_names_indicator["NeighborhoodToCell"] == T){
+    ## 1. Move the neighbor graph construction here
+    ## 2. Enable a k-nearest-neighbor parameter as an alternative
+    edgelist <- compute_edgelist(sys.small,position.x,position.y,k,rad.set)
+  }
+  
+  
+  # Calculate NICHES organizations without spatial restrictions
+  # jc: only pass the processed data to each function
+  if (CellToCell == T){output[[length(output)+1]] <- RunCellToCell(sys.small=sys.small,
+                                                                   ground.truth=ground.truth,
+                                                                   assay = "RNA",
+                                                                   meta.data.to.map = colnames(meta.data.to.map)
+  )}
+  if (CellToSystem == T){output[[length(output)+1]] <- RunCellToSystem(sys.small=sys.small,
+                                                                       ground.truth=ground.truth,
+                                                                       assay = "RNA",
+                                                                       meta.data.to.map = colnames(meta.data.to.map),
+                                                                       blend = blend
+  )}
+  if (SystemToCell == T){output[[length(output)+1]] <- RunSystemToCell(sys.small=sys.small,
+                                                                       ground.truth=ground.truth,
+                                                                       assay = "RNA",
+                                                                       meta.data.to.map = colnames(meta.data.to.map),
+                                                                       blend = blend
+  )}
+  
+  
+  if (CellToCellSpatial == T){output[[length(output)+1]] <- RunCellToCellSpatial(sys.small=sys.small,
+                                                                                 ground.truth=ground.truth,
+                                                                                 assay = "RNA",
+                                                                                 meta.data.to.map = colnames(meta.data.to.map),
+                                                                                 edgelist = edgelist
+  )} #Spatially-limited Cell-Cell vectors
+  if (CellToNeighborhood == T){output[[length(output)+1]] <- RunCellToNeighborhood(sys.small=sys.small,
+                                                                                   ground.truth=ground.truth,
+                                                                                   assay = "RNA",
+                                                                                   meta.data.to.map = colnames(meta.data.to.map),
+                                                                                   edgelist = edgelist
+  )} #Spatially-limited Cell-Neighborhood vectors
+  if (NeighborhoodToCell == T){output[[length(output)+1]] <- RunNeighborhoodToCell(sys.small=sys.small,
+                                                                                   ground.truth=ground.truth,
+                                                                                   assay = "RNA",
+                                                                                   meta.data.to.map = colnames(meta.data.to.map),
+                                                                                   edgelist = edgelist
+  )} #Spatially-limited Neighborhood-Cell vectors (niches)
+  
   # jc: Add organization names to the list
   names(output) <- names(org_names_indicator)[org_names_indicator]
   
